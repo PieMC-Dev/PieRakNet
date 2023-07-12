300a2b1b571128b0ae026cfda885bd3082e1cb95
soyelmismo <yo@resisto.rodeo>
2023-07-12 09:29:27 -0500

    check bytes instance for encoding

--- pieraknet/packets/offline_pong.py
+++ pieraknet/packets/offline_pong.py
@@ -13,10 +13,10 @@ class OfflinePong(Packet):
     def encode_payload(self):
         self.write_long(self.client_timestamp)
         self.write_long(self.server_guid)
-        if not (self.magic is bytes):
+        if not isinstance(self.magic, bytes):
             self.magic: bytes = self.magic.encode('utf-8')
         self.write_magic(self.magic)
-        if not (self.server_name is bytes):
+        if not isinstance(self.server_name, bytes):
             self.server_name: bytes = self.server_name.encode('utf-8')
         self.write_string(self.server_name)
 
