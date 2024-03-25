1ae16397b486c5032a9bfe9c4f2fddf1253289ca
andiricum2 <andiricum2@gmail.com>
2024-03-25 16:40:12 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 def readme():
-    with open('README.md', 'r') as f:
+    with open('README.md', 'r', encoding='utf-8') as f:
         return f.read()
 
 
